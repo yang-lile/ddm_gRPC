294a7fb7fa4731d34caf7a57de8f4de8f190a6ae
yang-lile <liluaya@foxmail.com>
2021-02-18 00:26:11 +0800

    add io from json

--- bin/ddm_gRPC.dart
+++ bin/ddm_gRPC.dart
@@ -1,3 +1,37 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:ddm_gRPC/data/rulers.dart';
+import 'package:ddm_gRPC/generated/meta_data/meta_data.pb.dart';
+
+// io from json
 void main(List<String> arguments) {
-  print('Hello world!');
+  // mainWrite();
+  // mainRead();
+}
+
+void mainWrite() {
+  var file = File('json/db.json');
+  file.writeAsStringSync(
+    jsonEncode(
+      StaticDataPool.rulers,
+      toEncodable: (value) {
+        return (value as Ruler).writeToJson();
+      },
+    ),
+    mode: FileMode.write,
+  );
+}
+
+void mainRead() {
+  var data = List<Ruler>.empty(growable: true);
+  var file = File('json/db.json');
+  jsonDecode(file.readAsStringSync(), reviver: (a, b) {
+    if (a == null) {
+      return null;
+    }
+    data.add(Ruler.fromJson(b));
+    return null;
+  });
+  print(data[1].instruction);
 }
