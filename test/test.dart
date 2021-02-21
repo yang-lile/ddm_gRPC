5c8ded51275222832770c8035f4fb7ba6b989c5c
yang-lile <liluaya@foxmail.com>
2021-02-21 18:19:25 +0800

    remove test docker server

--- test/test.dart
+++ test/test.dart
@@ -1,7 +1,6 @@
 import 'package:ddm_gRPC/data/rulers.dart';
 import 'package:ddm_gRPC/ddm_server.dart';
 import 'package:ddm_gRPC/generated/meta_data/meta_data.pbgrpc.dart';
-import 'package:ddm_gRPC/ip_data.dart';
 import 'package:test/test.dart';
 import 'package:grpc/grpc.dart' as grpc;
 
@@ -30,26 +29,6 @@ void main({List<String> args}) {
           expect(await result.toList(), StaticDataPool.rulers);
         },
       );
-    },
-  );
-
-  test(
-    'from docker server',
-    () async {
-      final channel = grpc.ClientChannel(
-        IpData.host,
-        port: IpData.port,
-        options: const grpc.ChannelOptions(
-          credentials: grpc.ChannelCredentials.insecure(),
-        ),
-      );
-      final stub = DDMClient(
-        channel,
-        options: grpc.CallOptions(timeout: Duration(seconds: 30)),
-      );
-
-      final result = stub.getRulers(Empty());
-      expect(await result.toList(), StaticDataPool.rulers);
     },
   );
   return;
