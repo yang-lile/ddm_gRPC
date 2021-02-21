bba1a381cab449540ee42e1d097fed29b879179e
yang-lile <liluaya@foxmail.com>
2021-02-21 17:16:02 +0800

    fix dart_test.yaml

--- test/test.dart
+++ test/test.dart
@@ -5,28 +5,31 @@ import 'package:test/test.dart';
 import 'package:grpc/grpc.dart' as grpc;
 
 void main({List<String> args}) {
-  group('server start', () {
-    test(
-      'get server',
-      () async {
-        await startServer();
+  group(
+    'server start',
+    () {
+      test(
+        'get server',
+        () async {
+          await startServer();
 
-        final channel = grpc.ClientChannel(
-          'localhost',
-          port: 8080,
-          options: const grpc.ChannelOptions(
-            credentials: grpc.ChannelCredentials.insecure(),
-          ),
-        );
-        final stub = DDMClient(
-          channel,
-          options: grpc.CallOptions(timeout: Duration(seconds: 30)),
-        );
+          final channel = grpc.ClientChannel(
+            'localhost',
+            port: 8080,
+            options: const grpc.ChannelOptions(
+              credentials: grpc.ChannelCredentials.insecure(),
+            ),
+          );
+          final stub = DDMClient(
+            channel,
+            options: grpc.CallOptions(timeout: Duration(seconds: 30)),
+          );
 
-        final result = stub.getRulers(Empty());
-        expect(await result.toList(), StaticDataPool.rulers);
-      },
-    );
-  });
+          final result = stub.getRulers(Empty());
+          expect(await result.toList(), StaticDataPool.rulers);
+        },
+      );
+    },
+  );
   return;
 }
